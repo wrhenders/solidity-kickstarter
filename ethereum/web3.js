2d5af9084625f51c23bdde229e8cc5ae1d5926ac
Ryan Henderson <wrhenders@gmail.com>
2022-04-15 12:37:07 -0500

    added semantic form

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,11 +3,9 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-  // We are in the browser and metamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
   web3 = new Web3(window.ethereum);
 } else {
-  // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
     "https://rinkeby.infura.io/v3/49c1b6f4d5e24da3a9871abaebb9a071"
   );
