202039b5f0bba7da5b389561773aa62cdd5c8afc
Ryan Henderson <wrhenders@gmail.com>
2022-04-18 10:26:57 -0500

    added contribute form and Campaign formatting

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { Card, Grid } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import CampaignSearch from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -8,14 +11,66 @@ class CampaignShow extends Component {
 
     const summary = await campaign.methods.getSummary().call();
 
-    console.log(summary);
-    return {};
+    return {
+      address: props.query.address,
+      minContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
+  renderCards() {
+    const { balance, manager, minContribution, requestsCount, approversCount } =
+      this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this comapaign and can create requests to withdraw",
+        style: { overflowWrap: "break-word" },
+      },
+      {
+        header: minContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by majority of Approvers.",
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description:
+          "Total number of users who have already contributed to the contract",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ETH)",
+        description: "The balance of money this campaign has to distribute",
+      },
+    ];
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        <Grid>
+          <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }
